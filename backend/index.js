ef86ea66b102f311fd66709edd55c8c024eccc31
Snehal-Salvi <snehalsalvi099@gmail.com>
2024-06-13 22:53:55 +0530

    added signup and signin routes in backend

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import { connectToDb } from "./config/mongoose.js";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("App is working");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(3001, () => {
+  console.log("Server is listening on port 3001");
+  connectToDb();
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
